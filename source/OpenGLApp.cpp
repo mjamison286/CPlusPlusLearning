8e1bfd54a46e1c291cccd6a91ab24e75b891c298
mjamison286 <mjamison286@gmail.com>
2026-04-12 02:18:56 -0500

    broke the working program by trying to use modern opengl rather than immediate

--- source/OpenGLApp.cpp
+++ source/OpenGLApp.cpp
@@ -35,27 +35,34 @@ int main(void)
 
     glfwShowWindow(window);
 
-    //GLenum err = glewInit();
+    float positions[6] = 
+    {
+        -0.5f, -0.5f, 0.0f, 0.5f, 0.5f, -0.5f
+    };
+
+    unsigned int buffer;
+    glGenBuffers(1, &buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+
+    glEnableVertexAttribArray(0);
+
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
 
     while(!glfwWindowShouldClose(window))
     {
         glClear(GL_COLOR_BUFFER_BIT);
 
-        int width, height;
-
-        glfwGetFramebufferSize(window, &width, &height);
-
-        glBegin(GL_TRIANGLES);
-        glVertex2f(-0.5f, -0.5f);
-        glVertex2f(0.0f, 0.5f);
-        glVertex2f(0.5f, -0.5f);
-        glEnd();
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         glfwSwapBuffers(window);
 
         glfwPollEvents();
 
         //this should keep it open right?
+        //yes, yes it does
     }
 
     return 0;
